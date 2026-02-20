f099e93c3fb0439ce223cb284fb533a6ab7e145d
Christopher1078 <christortizfun@gmail.com>
2026-02-20 08:42:19 -0600

    Clase Base Hecha

--- src/exameni_labprogii/RentItem.java
+++ src/exameni_labprogii/RentItem.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package exameni_labprogii;
+import java.util.Calendar;
+import javax.swing.ImageIcon;
+public abstract class RentItem {
+    protected String codigo, nombre;
+    protected double precioBase;
+    protected int copiasDisp;
+    protected ImageIcon imagenItem = new ImageIcon();
 
-/**
- *
- * @author USER
- */
-public class RentItem {
+    public RentItem(String codigo, String nombre, double precioBase) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precioBase = precioBase;
+        this.copiasDisp=0;
+    }
+    
+    @Override
+    public String toString(){
+        return "Codigo: "+codigo+"\nNombre del item: "+nombre+"\nPrecio base: "+precioBase+"\nCantidad de copias: "+copiasDisp;
+    }
+    
+    abstract double pagoRenta(int dias);
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+    
     
 }
